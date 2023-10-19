d7df37d4f0f1f4c0774fc5b88d2dc8d120faf11c
Kushalappa C A <kushalappa.ca@Incture.local>
2023-10-20 00:06:52 +0530

    achieved basic authentication api call from react front end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
+import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
+
+
+function submit() {
+  const username = 'ramesh';
+  const password = 'ramesh';
+  const credentials = `${username}:${password}`;
+  const base64Credentials = btoa(credentials);
+  const apiUrl = 'http://localhost:8080/usr';
+  const requestOptions = {
+    method: 'GET',
+    headers: {
+      'Authorization': `Basic ${base64Credentials}`,
+    }
+  }
+
+  fetch(apiUrl, requestOptions)
+    .then((response) => {
+      if (response.ok) {
+        console.log(response.text());
+      }
+    })
+}
 
 function App() {
+
+  const textFieldStyle = {
+    width: '20rem'
+  };
+
+  const useStyles = makeStyles((theme) => ({
+    topMargin: {
+      marginTop: '200px', // Adjust the margin as needed
+    },
+  }));
+
+  const buttonStyle = {
+    width: '20rem'
+  }
+
+  const marginTop = {
+    margin: '50 rem'
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1 style={marginTop}>ChatApp</h1>
+
+      <TextField
+        required
+        id="outlined-required"
+        label="Required"
+        placeholder='Username'
+        style={textFieldStyle}
+
+      />
+      <br></br>
+      <TextField
+        id="outlined-password-input"
+        label="Password"
+        type="password"
+        autoComplete="current-password"
+        style={textFieldStyle}
+      />
+      <br></br>
+      <br></br>
+      <Button variant="contained" style={buttonStyle} onClick={submit}>Login</Button>
     </div>
   );
 }
